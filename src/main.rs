54d19ed8ab1af5a5aee7bdeab31b41253ce7a62a
David Fox <david@weve.security>
2025-08-28 17:43:09 +0100

    create hello and goodbye functions

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
-/// A hello world program
+/// A "Hello, world!" program.
+///
+/// This is the best implementation of this program to ever exist.
 
 fn main() {
+    print_hello();
+    print_goodbye();
+}
+
+fn print_hello() {
     println!("Hello, world!");
+}
+
+fn print_goodbye() {
     println!("Goodbye, world!");
 }
